50c6f187c29c3eab72081ae3485bf732c93f15e6
RITESH KUMAR SHUKLA <riteshshukla2381@gmail.com>
2022-03-09 17:20:53 +0530

    Initial

---
+++ OCR/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+from django.shortcuts import render,HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from PIL import Image
+import pytesseract
+
+# Create your views here.
+@csrf_exempt
+def index(request):
+    s=""
+    if (request.method=="POST"):
+        img=request.FILES["answersheet"]
+        img=Image.open(img)
+        s=(pytesseract.image_to_string(img))
+        
+
+    return render(request,"index.html",{"s":s})
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Steps
+1) Download or clone this repository.  
+2) Download & install the tesseract software from https://digi.bib.uni-mannheim.de/tesseract/'.  Also add it to path.  
+3) Create a virtual enviroment. (python -m venv env).  
+4) Activate the enviroment.(./env/Scripts/activate) 
+5) Install the requirements by typing pip install -r requirements.txt. 
+6) Run the server(python manage.py runserver). 
+
+## Enjoii 🌚
